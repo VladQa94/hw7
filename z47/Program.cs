5061fb420aef3db84eaa0805163c0b1e50de3ab8
Vladislav <vladislav_chernikov94@mail.ru>
2022-12-20 19:04:37 +0700

    z47

---
+++ z47/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.WriteLine("Введите количество строк");
+int lengthRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int lengthCol = Convert.ToInt32(Console.ReadLine());
+
+double[,] numbers = new double[lengthRow, lengthCol];
+
+void generate2DArray(double[,] array)
+{
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+}
+
+
+void print2DArray(double[,] array)
+{
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+generate2DArray(numbers);
+print2DArray(numbers);
